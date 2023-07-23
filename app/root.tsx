fd82ffdacacddea5df728125ea43859ca4d2cf82
Phil <phil@upvalue.io>
2023-07-22 18:09:37 -0700

    links

--- app/root.tsx
+++ app/root.tsx
@@ -1,4 +1,13 @@
-import { LiveReload, Meta, Outlet, Scripts, ScrollRestoration } from "@remix-run/react";
+import { cssBundleHref } from "@remix-run/css-bundle";
+import { Links, LiveReload, Meta, Outlet, Scripts, ScrollRestoration } from "@remix-run/react";
+import { LinksFunction } from "@remix-run/server-runtime";
+
+import stylesheet from "~/tailwind.css";
+
+export const links: LinksFunction = () => [
+  { rel: "stylesheet", href: stylesheet },
+  ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
+];
 
 export default function App() {
   return (
@@ -7,6 +16,7 @@ export default function App() {
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width,initial-scale=1" />
         <Meta />
+        <Links />
       </head>
       <body>
         <Outlet />
